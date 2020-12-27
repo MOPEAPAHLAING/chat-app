8aa532f5490a06e945cfa871e15799aca4871639
moepapahlaing <moepapahlaing21@gmail.com>
2020-12-27 11:49:24 +0630

    initialize firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase form 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyAmgQ5Mol8DWcwnAkyuTX2AWBpQe-FN3QM",
+    authDomain: "chat-web-app-feef6.firebaseapp.com",
+    databaseURL: "https://chat-web-app-feef6-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chat-web-app-feef6",
+    storageBucket: "chat-web-app-feef6.appspot.com",
+    messagingSenderId: "731805380337",
+    appId: "1:731805380337:web:37f981f6eb9455fb9cc350"
+  };
+
+const app = firebase.initialezeApp(config);
