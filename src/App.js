ce4514c2aad0bda7a9283aeed949c4e764732361
moepapahlaing <moepapahlaing21@gmail.com>
2020-12-27 11:03:25 +0630

    scaffold the app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss';
+
+const App = () => {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default App
+
