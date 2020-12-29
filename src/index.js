e7595e6950bd341df6e7dc152bcd8eaa2004ad1b
moepapahlaing <moepapahlaing21@gmail.com>
2020-12-29 11:27:55 +0630

    created public and private routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BrowserRouter from 'react-router-dom';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
