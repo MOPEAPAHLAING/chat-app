82963597c7a304ef50c8959d95f834ab7140b0a1
moepapahlaing <moepapahlaing21@gmail.com>
2021-01-02 17:59:22 +0630

    created rooms context

---
+++ src/context/rooms.context.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable arrow-body-style */
+import React, {createContext, useState, useEffect} from 'react'
+import { database } from '../misc/firebase';
+import { transformToArrWithId } from '../misc/helper';
+
+const RoomsContext = createContext();
+
+export const RoomsProvider = ({children}) => {
+    const [rooms, setRooms] = useState(null);
+
+    useEffect(() => {
+        const roomListRef = database.ref('rooms');
+
+        roomListRef.on('value', (snap) => {
+            const data = transformToArrWithId(snap.val());
+            setRooms(data)
+        })
+
+        return () => {
+            roomListRef.off()
+        }
+    }, [])
+
+    return (
+        <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
+    )
+}
