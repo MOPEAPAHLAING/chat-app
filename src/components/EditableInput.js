24f96a8b15465990821724e028d6160e07f90754
moepapahlaing <moepapahlaing21@gmail.com>
2021-01-04 17:24:52 +0630

    added edit room drawer

--- src/components/EditableInput.js
+++ src/components/EditableInput.js
@@ -3,7 +3,14 @@
 import React, { useCallback, useState } from 'react'
 import { Alert, Icon, Input, InputGroup } from 'rsuite'
 
-const EditableInput = ({initialValue, onSave, label = null, placeholder="Write your value", emptyMsg="Input is empty", ...inputProps}) => {
+const EditableInput = ({
+    initialValue, 
+    onSave, 
+    label = null, 
+    placeholder="Write your value", 
+    emptyMsg="Input is empty", 
+    wrapperClassName = "",
+    ...inputProps}) => {
     const [input, setInput] = useState(initialValue);
     const [isEditable, setIsEditable] = useState(false);
 
@@ -31,7 +38,7 @@ const EditableInput = ({initialValue, onSave, label = null, placeholder="Write y
     }
     
     return (
-        <div>
+        <div className={wrapperClassName}>
             {label}
             <InputGroup>
                 <Input
