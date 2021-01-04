14d61d055652d7c85bc80fdfa4b4e113d0b199ab
moepapahlaing <moepapahlaing21@gmail.com>
2021-01-04 11:01:35 +0630

    update all user references if data is updated

--- src/misc/helper.js
+++ src/misc/helper.js
@@ -14,3 +14,21 @@ export function transformToArrWithId(snapVal) {
         return {...snapVal[roomId], id: roomId}
     }) : []
 }
+
+export async function getUserUpdates(userId, keyToUpdate, value, db) {
+    const updates = {};
+    updates[`/profiles/${userId}/${keyToUpdate}`] = value;
+    const getMesgs = db.ref('/messages').orderByChild('author/uid').equalTo(userId).once('value');
+    const getRooms = db.ref('/rooms').orderByChild('lastMessage/author/uid').equalTo(userId).once('value');
+    const [mSnap, rSnap] = await Promise.all([getMesgs, getRooms]);
+    
+    mSnap.forEach(msgSnap => {
+        updates[`/messages/${msgSnap.key}/author/${keyToUpdate}`] = value;
+    })
+
+    rSnap.forEach(roomSnap => {
+        updates[`/rooms/${roomSnap.key}/lastMessage/author/${keyToUpdate}`] = value;
+    })
+
+    return updates;
+}
